7e55dd82e79b5a9596ec106972ed292f03a6b0cb
Josué Silva <josuesilvarosa20@hotmail.com>
2024-03-10 23:17:54 -0300

    Projeto em andamento

--- src/main/java/com/job/demo/jobalexa/controller/ControllerCompany.java
+++ src/main/java/com/job/demo/jobalexa/controller/ControllerCompany.java
@@ -1,11 +1,15 @@
 package com.job.demo.jobalexa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("company")
-public class Company {
+public class ControllerCompany {
+
 
 
 }
